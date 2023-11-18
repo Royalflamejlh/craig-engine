b1721aa2d230f45243148ad9e6827e5d51343b05
Rooyal <49204095+Royalflamejlh@users.noreply.github.com>
2023-11-18 02:27:32 -0600

    saved board file

--- board.h
+++ board.h
@@ -1,4 +1,4 @@
 extern void initializeBoard();
 void freeBoard(char** board);
 void printBoard(char board[8][8]);
-void recieveMove(char board[8][8], char* move);
+void recieveMove(char board[8][8], int64_t move);
