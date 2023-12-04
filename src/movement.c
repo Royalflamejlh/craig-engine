4c1399fd1471bb2cdfd59a491027f9d6573221a5
Royalflamejlh <john@thehowardfamily.net>
2023-12-04 22:17:56 +0000

    woopidtyu

--- src/movement.c
+++ src/movement.c
@@ -1,8 +1,8 @@
 #include "types.h"
 #include "./bitboard/bitboard.h"
 
-static void generateWhiteMoves(Position position, Move* moveList, int* size);
-static void generateBlackMoves(Position position, Move* moveList, int* size);
+static uint64_t generateWhiteMoves(Position position, Move* moveList, int* size);
+static uint64_t generateBlackMoves(Position position, Move* moveList, int* size);
 
 
 void generateMoves(Position position,  Move* moveList, int* size){
@@ -11,7 +11,7 @@ void generateMoves(Position position,  Move* moveList, int* size){
 
     if(position.flags & WHITE_TURN){
         printf("Generating moves for white\n");
-        generateWhiteMoves(position, moveList, size);
+        printBB(generateWhiteMoves(position, moveList, size));
     }
     else{
         printf("Generating moves for black\n");
@@ -20,7 +20,7 @@ void generateMoves(Position position,  Move* moveList, int* size){
 
 }
 
-static void generateWhiteMoves(Position position, Move* moveList, int* size){
+static uint64_t generateWhiteMoves(Position position, Move* moveList, int* size){
     //Check for hashed moves
     //Check for capturing moves
     //Check for killer moves
@@ -33,12 +33,21 @@ static void generateWhiteMoves(Position position, Move* moveList, int* size){
     attack_mask |= getKnightMovesAppend(position.w_knight, position.white, moveList, size);
     attack_mask |= getKingMovesAppend(position.w_king, position.white, moveList, size);
     attack_mask |= getPawnMovesAppend(position.w_pawn, position.white, position.black, position.en_passant, position.flags, moveList, size);
+    return attack_mask;
 }
 
-static void generateBlackMoves(Position position, Move* moveList, int* size){
+static uint64_t generateBlackMoves(Position position, Move* moveList, int* size){
     //Check for hashed moves
     //Check for capturing moves
     //Check for killer moves
     //Check rest of moves
-    getBishopMovesAppend(position.b_bishop, position.black, position.white, moveList, size);
+    uint64_t attack_mask = 0ULL;
+    attack_mask |= getBishopMovesAppend(position.b_bishop, position.black, position.white, moveList, size);
+    attack_mask |= getRookMovesAppend(position.b_rook, position.black, position.white, moveList, size);
+    attack_mask |= getBishopMovesAppend(position.b_queen, position.black, position.white, moveList, size);
+    attack_mask |= getRookMovesAppend(position.b_queen, position.black, position.white, moveList, size);
+    attack_mask |= getKnightMovesAppend(position.b_knight, position.black, moveList, size);
+    attack_mask |= getKingMovesAppend(position.b_king, position.black, moveList, size);
+    attack_mask |= getPawnMovesAppend(position.b_pawn, position.black, position.white, position.en_passant, position.flags, moveList, size);
+    return attack_mask;
 }
