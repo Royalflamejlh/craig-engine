92bd786fc0fa69162dd65d6c892347f54ce735d2
Royalflamejlh <john@thehowardfamily.net>
2023-12-16 19:14:55 +0000

    this engine is a large pooper

--- src/evaluator.c
+++ src/evaluator.c
@@ -1,18 +1,22 @@
 #include "evaluator.h"
+#include "util.h"
 
-#define KING_VALUE     100 000
-#define QUEEN_VALUE     10 000
-#define ROOK_VALUE       5 000
-#define BISHOP_VALUE     3 550
-#define KNIGHT_VALUE     3 500
-#define PAWN_VALUE       1 000
+#define KING_VALUE     100000
+#define QUEEN_VALUE     10000
+#define ROOK_VALUE       5000
+#define BISHOP_VALUE     3550
+#define KNIGHT_VALUE     3500
+#define PAWN_VALUE       1000
 
-#define ATTACK_BONUS        10  //Elo for each square under attack
-#define DEFEND_BONUS       200  //Elo for each defended piece
-#define CHECK_PEN          100  //Elo lost under check
+#define ATTACK_BONUS       10  //Eval for each square under attack
+#define D_ATTACK_BONUS    100  //Eval for each enemy under attack
+#define DEFEND_BONUS      200  //Eval for each defended piece
+#define CHECK_PEN         100  //Eval lost under check
+#define TURN_BONUS          5
 
-#define DOUBLE_PAWN_PEN    200  //Elo lost for double pawns
-#define ISO_PAWN_PEN       100  //Elo lost for isolated pawns
+
+#define DOUBLE_PAWN_PEN   200  //Eval lost for double pawns
+#define ISO_PAWN_PEN      100  //Eval lost for isolated pawns
 
 
 
@@ -20,12 +24,33 @@ int evaluate(Position pos){
     int eval_val = 0;
     int turn = pos.flags & WHITE_TURN;
 
-    if(pos.flags & IN_CHECK) eval_val -= CHECK_PEN;
+    if(pos.flags & IN_CHECK) eval_val += turn ? -CHECK_PEN : CHECK_PEN;
+
+    eval_val += turn ? TURN_BONUS : -TURN_BONUS;
 
     //Pieces
-    for(int i = 0; i < 64; i++){
-        
-    }
+    eval_val += KING_VALUE * count_bits(pos.king[1]);
+    eval_val += QUEEN_VALUE * count_bits(pos.queen[1]);
+    eval_val += ROOK_VALUE * count_bits(pos.rook[1]);
+    eval_val += BISHOP_VALUE * count_bits(pos.bishop[1]);
+    eval_val += KNIGHT_VALUE * count_bits(pos.knight[1]);
+    eval_val += PAWN_VALUE * count_bits(pos.pawn[1]);
 
-    return turn ? eval_val : -eval_val;
+    eval_val -= KING_VALUE * count_bits(pos.king[0]);
+    eval_val -= QUEEN_VALUE * count_bits(pos.queen[0]);
+    eval_val -= ROOK_VALUE * count_bits(pos.rook[0]);
+    eval_val -= BISHOP_VALUE * count_bits(pos.bishop[0]);
+    eval_val -= KNIGHT_VALUE * count_bits(pos.knight[0]);
+    eval_val -= PAWN_VALUE * count_bits(pos.pawn[0]);
+
+    eval_val += DEFEND_BONUS * count_bits(pos.color[1] &  pos.attack_mask[1]);
+    eval_val += ATTACK_BONUS * count_bits(pos.attack_mask[1]);
+    eval_val += D_ATTACK_BONUS * count_bits(pos.color[0] &  pos.attack_mask[1]);
+
+    eval_val -= DEFEND_BONUS * count_bits(pos.color[0] &  pos.attack_mask[0]);
+    eval_val -= ATTACK_BONUS * count_bits(pos.attack_mask[0]);
+    eval_val -= D_ATTACK_BONUS * count_bits(pos.color[1] &  pos.attack_mask[0]);
+
+    return eval_val;
 }
+
--- src/util.h
+++ src/util.h
@@ -7,4 +7,12 @@ uint64_t perft(int depth, Position pos);
 int checkMoveCount(Position pos);
 int python_init();
 int python_close();
+
+static inline int count_bits(uint64_t v){
+    unsigned int c;
+    for (c = 0; v; c++){
+        v &= v - 1;
+    }
+    return c;
+}
 #endif
