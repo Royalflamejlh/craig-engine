7bd89f4c7b600be2c84f3f7a65f2722e3175b4a0
Rooyal <49204095+Royalflamejlh@users.noreply.github.com>
2025-10-25 12:08:56 -0500

    Adding static analysis

--- src/nnue/nnue.c
+++ src/nnue/nnue.c
@@ -1,9 +1,9 @@
 #include "../types.h"
 
 
-i32 nnue_position(Position *pos){
-    for(u8 c = 0; c < sizeof(Position) / sizeof(u8); c++){
-        pos + 
-        printf("%d", )
-    }
-}
+// i32 nnue_position(Position *pos){
+//     for(u8 c = 0; c < sizeof(Position) / sizeof(u8); c++){
+//         pos + 
+//         printf("%d", )
+//     }
+// }
